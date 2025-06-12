6ad894f0ff6bc67ea3e6c1cba570d0d0527e0fb8
oleh-tft <tiftelalbion@gmail.com>
2025-06-12 23:53:54 +0300

    idk

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -1,17 +1,14 @@
 import React from "react";
 import { Header } from "./Header";
-import './Landing.css'
+import './Landing.css';
 
 export function Landing() {
 
     return (
         <div id="landing">
-            {/* <div className="bg">
-                <div className="bggrad">
-                    <img className="bgfit" src="landingbg.jpg"/>
-                </div>
-            </div> */}
-            <Header></Header>
+            <div className="bg">
+                <Header></Header>
+            </div>
         </div>
     );
 
