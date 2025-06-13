7eda2078b27445ff2f7e9a406d9233ce7193b8d0
oleh-tft <tiftelalbion@gmail.com>
2025-06-13 16:25:57 +0300

    footer fix

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -1,14 +1,19 @@
 import React from "react";
 import { Header } from "./Header";
+import { Footer } from "./Footer";
+
 import './Landing.css';
 
 export function Landing() {
 
     return (
-        <div id="landing">
-            <div className="bg"></div>
-            <div className="faded"></div>
-            <Header></Header>
+        <div>
+            <div id="landing">
+                <div className="bg"></div>
+                <div className="faded"></div>
+                <Header></Header>
+            </div>
+            <Footer></Footer>
         </div>
     );
 
