91a5f96acbb0901d16bd401659807628942b3832
Arina <arina.krapivnaya@gmail.com>
2025-06-13 22:53:15 +0300

    404

---
+++ src/components/Error.js
@@ -0,0 +1,39 @@
+import React from "react";
+import './Error.css';
+import { Logo } from "./Logo";
+import { ButtonMain } from "./ButtonMain";
+
+
+export function Error() {
+
+    return (
+        <div id="error"> 
+        <div className = "logocentered">
+            <Logo></Logo>
+        </div>
+            <div className="bgerror"></div>
+            <div className="fadederror"></div>
+
+            <div className="header"></div>
+           <div className ="textblock">
+
+            <div className="titleerror">Lost your way?</div>
+            <div className="texterror">Sorry, we can't find that page. You'll find lots to explore on the home page. </div>
+<ButtonMain 
+// bg ="white"
+// color="black"
+// padding="10px 20px"
+ text="Netflix Home"></ButtonMain>
+
+<div className="codeerror">
+    <div className ="stick"></div>
+    <div className ="codetext">Error Code</div>
+    <div className ="codetext" style={{ fontWeight: 700 }}>NSES-404</div>
+
+
+</div>
+           </div>
+        </div>
+    );
+
+}
