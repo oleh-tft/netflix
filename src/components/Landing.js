cb7835bf69b91cc2dda469a2fd6f29b91e5da8a8
Shkurenko Vira <shkurenko.verra@gmail.com>
2025-06-16 16:40:16 +0300

    Доробила Landing page

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -1,8 +1,11 @@
 import React from "react";
 import { Header } from "./Header";
 import { Footer } from "./Footer";
+import { ButtonMain } from "./ButtonMain";
 
 import './Landing.css';
+import { FAQSection } from "./FAQ";
+import Emailinput from "./Emailinput";
 
 export function Landing() {
 
@@ -13,7 +16,31 @@ export function Landing() {
                 <div className="faded"></div>
                 <Header></Header>
             </div>
-            <Footer></Footer>
+            <main>
+            <div style={{paddingTop: "15%", paddingBottom: "15%"}}>
+                <div style={{color: "white", textAlign: "center", marginTop: "40px" }}>
+                <h1 style={{ marginBottom: "20px" }}>
+              Unlimited movies, TV shows and more
+                 </h1>
+                <p style={{ fontSize: "1.1rem", marginBottom: "20px" }}>
+               Watch anywhere. Cancel anytime.
+                 </p>
+                 <p style={{ fontSize: "1.1rem", marginBottom: "20px" }}>
+                 Ready to watch? Enter your email to create or restart your membership.
+                 </p>
+                 <div style={{ display: "flex", justifyContent: "center", flexWrap: "wrap", gap: "10px" }}>
+           <Emailinput></Emailinput>
+                      <ButtonMain color="red" text="Get Started"></ButtonMain>
+                 </div>
+               </div>
+            </div>
+            <div className="FAQ">
+                  <FAQSection></FAQSection>
+            </div>
+            </main>
+            <div className="Footer">
+                 <Footer></Footer>
+            </div>
         </div>
     );
 
