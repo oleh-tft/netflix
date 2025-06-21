7ce4d104fd48dc2c034cdee4359d8b107d33d19b
oleh-tft <tiftelalbion@gmail.com>
2025-06-21 16:47:11 +0300

    Added i18n-next for localization. Landing page tweaks

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,17 +1,20 @@
 import React from "react";
+import { useTranslation } from 'react-i18next';
 import { DropDown } from "./DropDown";
 import './Header.css'
 import { Logo } from "./Logo";
 import { ButtonMain } from "./ButtonMain";
 
 export function Header() {
+
+    const { t } = useTranslation();
     
     return (
         <div id="head">
             <Logo></Logo>
             <div className="d-flex align-items-center justify-content-between btncontainer">
-                <DropDown img="language.png" text="Українська"  ></DropDown>
-                <ButtonMain color="red" text="Sign In"></ButtonMain>
+                <DropDown minWidth='120px'></DropDown>
+                <ButtonMain color="red" text={t('header.button.1')} minWidth='80px'></ButtonMain>
             </div>
         </div>
     );
